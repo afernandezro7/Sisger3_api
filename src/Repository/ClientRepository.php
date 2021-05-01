a649ab14ecc935acac6869c114306133c87f9fb2
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-04-30 23:23:21 -0500

    adding ClientRepository methods

--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -19,6 +19,70 @@ class ClientRepository extends ServiceEntityRepository
         parent::__construct($registry, Client::class);
     }
 
+    /**
+     * @return Client[] Returns an array of Client objects
+     */
+    public function findClientByFullName()
+    {
+        $em = $this->getEntityManager();
+        $query = $em->createQuery('SELECT c FROM App\Entity\Client c ORDER BY c.createdAt DESC ');
+        return $query->getResult();
+    }
+
+    /**
+     * @return Client[] Returns an array of Client objects
+     */
+    public function findClientsByUserAndMonth($operator, $from, $to)
+    {
+        $em = $this->getEntityManager();
+        $query = $em->createQuery('SELECT c FROM App\Entity\Client c WHERE c.user = :operator AND c.createdAt BETWEEN :from AND :to');
+        $query->setParameter('operator', $operator);
+        $query->setParameter('from', $from);
+        $query->setParameter('to', $to);
+
+
+        return $query->getResult();
+    }
+
+    /**
+     * @return Client[] Returns an array of Client objects
+     */
+    public function findClientsWithNoUserOrderedDesc($user)
+    {
+        $em = $this->getEntityManager();
+        $query = $em->createQuery('SELECT c FROM App\Entity\Client c WHERE c.user != :user ORDER BY c.createdAt DESC');
+        $query->setParameter('user', $user);
+
+        return $query->getResult();
+    }
+
+    /**
+     * @return Client[] Returns an array of Client objects
+     */
+    public function findByUserOrderedDesc()
+    {
+        $em = $this->getEntityManager();
+        $query = $em->createQuery('SELECT c FROM App\Entity\Client c ORDER BY c.createdAt DESC');
+
+
+        return $query->getResult();
+    }
+
+    public function findBusquedaSimple($text)
+    {
+        $text = '%' . $text . '%';
+        $text1 = $text . '%';
+        $text2 = '%' . $text;
+
+
+        $em = $this->getEntityManager();
+        $query = $em->createQuery("SELECT r FROM App\Entity\Client r WHERE (r.firstName LIKE :query OR r.firstName LIKE :query1 OR r.firstName LIKE :query2 OR r.lastName LIKE :query OR r.lastName LIKE :query1 OR r.lastName LIKE :query2 OR CONCAT(r.firstName, ' ', r.lastName) LIKE :query OR CONCAT(r.firstName, ' ', r.lastName) LIKE :query1 OR CONCAT(r.firstName, ' ', r.lastName) LIKE :query2) ORDER BY r.createdAt DESC");
+        $query->setParameter('query', $text);
+        $query->setParameter('query1', $text1);
+        $query->setParameter('query2', $text2);
+        return $query->getResult();
+    }
+
     // /**
     //  * @return Client[] Returns an array of Client objects
     //  */
