59c8e83d0fccc031fc8883eb1745677421dca65a
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-04-30 23:27:24 -0500

    adding ConceptoRepository methods

--- src/Repository/ConceptoRepository.php
+++ src/Repository/ConceptoRepository.php
@@ -19,6 +19,29 @@ class ConceptoRepository extends ServiceEntityRepository
         parent::__construct($registry, Concepto::class);
     }
 
+    public function findXTipoXContenedor($tipo, $contenedor)
+    {
+        $em = $this->getEntityManager();
+        $query = $em->createQuery('SELECT c FROM App\Entity\Concepto c WHERE c.tipo = :tipo AND c.contenedor = :contenedor');
+        $query->setParameter('tipo', $tipo);
+        $query->setParameter('contenedor', $contenedor);
+        return $query->getResult();
+    }
+
+    /**
+     * @return Concepto[] Returns an array of Concepto objects
+     */
+    public function findBusquedaSimple($text)
+    {
+        $value = '%' . $text . '%';
+
+        $em = $this->getEntityManager();
+        $query = $em->createQuery("SELECT c, e, n FROM BackendBundle:Concepto c JOIN c.consignado e JOIN c.contenedor n WHERE c.sisgerCode LIKE :query OR CONCAT(e.firstName, ' ', e.lastName) LIKE :query OR e.dni LIKE :query OR e.passport LIKE :query ORDER BY c.fechaCreacion DESC");
+        $query->setParameter('query', $value);
+        /*$query->setMaxResults(1000);*/
+        return $query->getResult();
+    }
+
     // /**
     //  * @return Concepto[] Returns an array of Concepto objects
     //  */
